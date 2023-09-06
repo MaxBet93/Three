a988e457904cc65b64221efa78bcad7cb97fc6a4
MaxBet93 <114855423+MaxBet93@users.noreply.github.com>
2023-09-06 21:39:53 +0300

    Three

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const firstNumber = prompt ("First  Number");
+const secondNumber = prompt ("Second Number");
+const thirdNumber = prompt ("Third Number");
+
+const sum = (Number(firstNumber) + Number(secondNumber) + Number(thirdNumber)) / 3;
+
+alert(`${sum}`);
